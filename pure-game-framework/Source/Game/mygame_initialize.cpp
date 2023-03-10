7c1dab93475bca83ddc7139dd61dcb9b5938b038
vhan9999 <66200083+vhan9999@users.noreply.github.com>
2023-03-10 13:36:51 +0800

    bmp ok

--- pure-game-framework/Source/Game/mygame_initialize.cpp
+++ pure-game-framework/Source/Game/mygame_initialize.cpp
@@ -38,7 +38,7 @@ void CGameStateInit::OnBeginState()
 
 void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-
+	GotoGameState(GAME_STATE_RUN);		// 切換至GAME_STATE_RUN
 }
 
 void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
