a9dfc9cfc5e1d1537e50b46ebc8d2e2fd0a75a29
thaksin5 <114573651+thaksin5@users.noreply.github.com>
2023-03-11 03:04:52 +0800

    build movement

--- pure-game-framework/Source/Game/mygame_run.cpp
+++ pure-game-framework/Source/Game/mygame_run.cpp
@@ -14,6 +14,12 @@ using namespace game_framework;
 // This class is the game execution object of the game, and the main game programs are here
 /////////////////////////////////////////////////////////////////////////////
 
+bool isMoveLeft = false;
+bool isMoveRight = false;
+bool isMoveUp = false;
+bool isMoveDown = false;
+bool isAnimated = false;
+
 CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
 {
 }
@@ -26,26 +32,54 @@ void CGameStateRun::OnBeginState()
 {
 }
 
-void CGameStateRun::OnMove()							// 移動遊戲元素 move
+void CGameStateRun::OnMove()							// 移動遊戲元素 move (always loop)
 {
-	
+	if (isMoveLeft) {
+		player.SetTopLeft(player.GetLeft() - 3, player.GetTop());
+	}
+	if (isMoveRight) {
+		player.SetTopLeft(player.GetLeft() + 3, player.GetTop());
+	}
+
 }
 
 void CGameStateRun::OnInit() // 遊戲的初值及圖形設定 set initial value and image
 {	
- 	
- 	player.LoadBitmapByString({ "resources/image/player/player_jump.bmp"});
- 	player.SetTopLeft(10, 10);
+ 	player.LoadBitmapByString({ "resources/image/player/player_1.bmp", "resources/image/player/player_2.bmp" });
+	player.SetFrameIndexOfBitmap(0);
+ 	player.SetTopLeft(120, 300);
 }
 
 void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-	
+	if (nChar == VK_LEFT) {
+		isMoveLeft = true;
+		// animate moving  
+		player.SetFrameIndexOfBitmap(0);
+		player.SetAnimation(100, false);
+		player.SetFrameIndexOfBitmap(1);
+	}
+	if (nChar == VK_RIGHT) {
+		isMoveRight = true;
+		// animate moving  
+		player.SetFrameIndexOfBitmap(0);
+		player.SetAnimation(100, false);
+		player.SetFrameIndexOfBitmap(1);
+	}
 }
 
 void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 {
-	
+	if (nChar == VK_LEFT) {
+		isMoveLeft = false;
+		// stop animate moving
+		player.SetAnimation(100, true);
+	}
+	if (nChar == VK_RIGHT) {
+		isMoveRight = false;
+		// stop animate moving
+		player.SetAnimation(100, true);
+	}
 }
 
 void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作 left mouse button (down)
