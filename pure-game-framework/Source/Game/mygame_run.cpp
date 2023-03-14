c059fb1c20a623748695aa3658c6c4577c47b026
thaksin5 <114573651+thaksin5@users.noreply.github.com>
2023-03-14 10:38:06 +0800

    build 3 ground brick

--- pure-game-framework/Source/Game/mygame_run.cpp
+++ pure-game-framework/Source/Game/mygame_run.cpp
@@ -9,9 +9,12 @@
 #include <vector>
 using namespace game_framework;
 
+
+
 /////////////////////////////////////////////////////////////////////////////
 // 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
 // This class is the game execution object of the game, and the main game programs are here
+// select multiple line = alt + shift + .
 /////////////////////////////////////////////////////////////////////////////
 
 bool isMoveLeft = false;
@@ -19,9 +22,16 @@ bool isMoveRight = false;
 bool isMoveUp = false;
 bool isMoveDown = false;
 bool isAnimated = false;
-int i = 0;
-std::vector<CMovingBitmap>  block_arr;
+int i = 0; // index loop vector
 
+// ground_brick array
+std::vector<CMovingBitmap> ground_brick_arr1; // ground_brick 1 
+std::vector<CMovingBitmap> ground_brick_arr2; // ground_brick 2
+std::vector<CMovingBitmap> ground_brick_arr3; // ground_brick 3
+
+/* ----CLASS---- */
+
+// Brick Factory
 class BrickFactory {
 public:
 	static CMovingBitmap createBrick(int type, int x, int y) {
@@ -40,6 +50,16 @@ public:
 	}
 };
 
+/* ----FUNCTION---- */
+void build_brick_horizontal(std::vector<CMovingBitmap> &ground_brick_arr, int type, int amount, int x, int y) {
+	CMovingBitmap brick;
+	for (int i = 0; i < amount; i++) { // ground brick up
+		brick = BrickFactory::createBrick(type, x, y);
+		ground_brick_arr.push_back(brick);
+		x += 32-2; // -2 pixel 
+	}
+}
+
 CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
 {
 }
@@ -53,7 +73,7 @@ void CGameStateRun::OnBeginState()
 	
 }
 
-void CGameStateRun::OnMove()							// 移動遊戲元素 move (always loop)
+void CGameStateRun::OnMove()  // 移動遊戲元素 move (always loop)
 {
 	// player movement
 	if (isMoveLeft) {
@@ -81,19 +101,22 @@ void CGameStateRun::OnInit() // 遊戲的初值及圖形設定 set initial value
 	player.SetFrameIndexOfBitmap(0);
  	player.SetTopLeft(100, 536-player.GetHeight()-32);
 
-	CMovingBitmap ground_brick2;
-	ground_brick.LoadBitmapByString({ "resources/image/object/block1/brown_brick3.bmp"}, RGB(64, 128, 128));
-	ground_brick.SetFrameIndexOfBitmap(0);
-	ground_brick.SetTopLeft(90,536-32);
-	
 	// build groundbrick
-	int block_posX = 0;
-	int block_posY = 536 - 32;
-	for (int i = 0; i < 2; i++) {
-		ground_brick2 = BrickFactory::createBrick(3, block_posX, block_posY);
-		block_arr.push_back(ground_brick2);
-		block_posX += 32;
-	}
+	CMovingBitmap ground_brick_up; // ground brick up
+	int groundX_up = 0;
+	int groundY_up = 536 - 32;
+	build_brick_horizontal(ground_brick_arr1, 3, 23, groundX_up, groundY_up);
+
+	CMovingBitmap ground_brick_mid; // ground brick mid
+	int groundX_mid = 0;
+	int groundY_mid = 536-2; // -2 ramaining pixel
+	build_brick_horizontal(ground_brick_arr2, 5, 23, groundX_mid, groundY_mid);
+
+	CMovingBitmap ground_brick_down; // ground brick mid
+	int groundX_down = 0;
+	int groundY_down = 566-2;
+	build_brick_horizontal(ground_brick_arr3, 5, 23, groundX_down, groundY_down);
+
 	// enemy
 	// enemy.LoadBitmapByString({ "resources/image/enemy/normal.bmp" }, RGB(64, 128, 128));
 	// enemy.SetFrameIndexOfBitmap(0);
@@ -155,8 +178,8 @@ void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動
 void CGameStateRun::OnShow()
 {	
 	player.ShowBitmap();
-	ground_brick.ShowBitmap();
-	for (auto i : block_arr) { // use 'auto' for iterate to avoid "signed/unsigned mismatch"
-		i.ShowBitmap();
-	}
+	// use 'auto' for iterate to avoid "signed/unsigned mismatch"
+	for (auto i : ground_brick_arr1) { i.ShowBitmap();} // ground brick up
+	for (auto i : ground_brick_arr2) { i.ShowBitmap();} //ground brick mid
+	for (auto i : ground_brick_arr3) { i.ShowBitmap(); } //ground brick down
 }
