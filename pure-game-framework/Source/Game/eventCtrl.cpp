3f9afd87b15ae718609fa55000f467850829cdd9
thaksin5 <114573651+thaksin5@users.noreply.github.com>
2023-05-18 22:48:20 +0800

    Revert "label map phase in eventCtrl"
    This reverts commit bd5184ee1d14c639d61a22b5662d65f4f9b0b68e.

--- pure-game-framework/Source/Game/eventCtrl.cpp
+++ pure-game-framework/Source/Game/eventCtrl.cpp
@@ -11,9 +11,8 @@
 using namespace game_framework;
 
 void CGameStateRun::EventCtrl() {
-	if (current_map == 1) { 
-		// phase2
-		if (player.enemy_shift_amount >= 600 && player.enemy_shift_amount <= 700 && !event_list["pipe_normal"]) { // event_list => add of event map
+	if (current_map == 1) {
+		if (player.enemy_shift_amount >= 600 && player.enemy_shift_amount <= 700 && !event_list["pipe_normal"]) {
 			Enemy normal3 = Enemy(890, 700, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal3.step_enemy_enemy_dead = true; normal3.speed_x = -2; enemys_arr.push_back(normal3);
 			event_list["pipe_normal"] = true;
 		}
@@ -21,13 +20,11 @@ void CGameStateRun::EventCtrl() {
 			Enemy up_seal_1(525, 600, { {"resources/image/enemy/seal.bmp"} }); up_seal_1.steel = false; up_seal_1.able_touch = false; up_seal_1.speed_y = -19; up_seal_1.step_enemy_player_dead = true; enemys_arr.push_back(up_seal_1);
 			event_list["up_seal_1"] = true;
 		}
-		// phase 4
 		if (player.enemy_shift_amount >= 2000 && player.enemy_shift_amount <= 2100 && !event_list["two_normal_1"]) {
 			Enemy normal3 = Enemy(890, 430, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal3.step_enemy_enemy_dead = true; normal3.speed_x = -2; enemys_arr.push_back(normal3);
 			Enemy normal4 = Enemy(1090, 230, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal4.step_enemy_enemy_dead = true; normal4.speed_x = -2; enemys_arr.push_back(normal4);
 			event_list["two_normal_1"] = true;
 		}
-		// phase 5
 		if (player.enemy_shift_amount >= 2780 && player.enemy_shift_amount <= 2880 && !event_list["down_seal_1"]) {
 			Enemy down_seal_1(630, -100, { {"resources/image/enemy/seal_flip.bmp"} }); down_seal_1.steel = false; down_seal_1.able_touch = false; down_seal_1.speed_y = 19; down_seal_1.step_enemy_player_dead = true; enemys_arr.push_back(down_seal_1);
 			event_list["down_seal_1"] = true;
@@ -45,13 +42,11 @@ void CGameStateRun::EventCtrl() {
 			}
 			event_list["three_fall_bricksss"] = true;
 		}
-		// phase 8
 		if (player.enemy_shift_amount >= 5122 && player.enemy_shift_amount <= 5222 && !event_list["two_normal_2"]) {
 			Enemy normal3 = Enemy(990, 700, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal3.step_enemy_enemy_dead = true; normal3.speed_x = -2; enemys_arr.push_back(normal3);
 			Enemy normal4 = Enemy(1140, 700, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal4.step_enemy_enemy_dead = true; normal4.speed_x = -2; enemys_arr.push_back(normal4);
 			event_list["two_normal_2"] = true;
 		}
-		// phase 10
 		if (player.enemy_shift_amount >= 6284 && player.enemy_shift_amount <= 6384 && !event_list["fall_normals"]) {
 			Enemy normal1 = Enemy(530, 0, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal1.step_enemy_enemy_dead = true; normal1.speed_x = -2; enemys_arr.push_back(normal1);
 			Enemy normal2 = Enemy(590, 0, { "resources/image/enemy/normal.bmp","resources/image/enemy/normal_flip.bmp" }); normal2.step_enemy_enemy_dead = true; normal2.speed_x = -2; enemys_arr.push_back(normal2);
@@ -63,7 +58,6 @@ void CGameStateRun::EventCtrl() {
 			Enemy yellow1(1000, 50, { {"resources/image/enemy/yellow_comet_flip.bmp"} }); yellow1.steel = false; yellow1.able_touch = false; yellow1.speed_x = -30; yellow1.step_enemy_player_dead = true; enemys_arr.push_back(yellow1);
 			event_list["yellow"] = true;
 		}
-		// phase 11
 		if (player.enemy_shift_amount >= 6900 && player.enemy_shift_amount <= 7000 && !event_list["down_seal_2"]) {
 			Enemy down_seal_2(600, -100, { {"resources/image/enemy/seal_flip.bmp"} }); down_seal_2.steel = false; down_seal_2.able_touch = false; down_seal_2.speed_y = 19; down_seal_2.step_enemy_player_dead = true; enemys_arr.push_back(down_seal_2);
 			event_list["down_seal_2"] = true;
